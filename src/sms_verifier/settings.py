0ca7148280dba1da7dac1cd9d2c48fd30cc20891
arielev <levinsonarie@gmail.com>
2019-10-21 11:34:06 +0300

    add init verifier application

--- src/sms_verifier/settings.py
+++ src/sms_verifier/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_settings.urls'
+ROOT_URLCONF = 'sms_verifier.urls'
 
 TEMPLATES = [
     {
---
+++ src/sms_verifier_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmsVerifierAppConfig(AppConfig):
+    name = 'sms_verifier_app'
